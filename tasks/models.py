ae7dde2f5754518d95711365bf4a0d4ec9c2ea97
joseedusarmento <j.eduardo.sarmento10@gmail.com>
2025-08-09 18:13:07 -0300

    implementa listagem de tarefas

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField(blank=True, null=True)
+    criada_em = models.DateTimeField(auto_now_add=True)
+    atualizada_em = models.DateTimeField(auto_now=True)
+    concluida = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titulo
+
+
 # Create your models here.
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Tarefa
 
-# Create your views here.
+def lista_tarefa(request):
+    tarefas = Tarefa.objects.all().order_by('-criada_em')
+    return render(request, 'todo_app/lista.html', {'tarefas': tarefas})
+
+def visualizar_tarefa(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    return render(request, 'todo_app/visualizar.html', {'tarefa': tarefa})
