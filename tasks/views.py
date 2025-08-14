9214212479817b0157baf16d040edde0ad087372
Ernesto <149084896+ErnestoSESB@users.noreply.github.com>
2025-08-14 13:28:49 -0300

    Atualizar o views.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Tarefa
+from .forms import TarefaForm
+
+def lista_tarefa(request):
+    tarefas = Tarefa.objects.all().order_by('-criada_em')
+    return render(request, 'todo_app/lista.html', {'tarefas': tarefas})
+
+def visualizar_tarefa(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    return render(request, 'todo_app/visualizar.html', {'tarefa': tarefa})
+
+def criar_tarefa(request):
+    if request.method == 'POST':
+        form = TarefaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_tarefas')
+    else:
+        form = TarefaForm()
+    return render(request, 'todo_app/form_tarefa.html', {'form': form, 'titulo': 'Nova Tarefa'})
+
+def editar_tarefa(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    if request.method == 'POST':
+        form = TarefaForm(request.POST, instance=tarefa)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_tarefas')
+    else:
+        form = TarefaForm(instance=tarefa)
+    return render(request, 'todo_app/form_tarefa.html', {'form': form, 'titulo': 'Editar Tarefa'})
+
+def deletar_tarefa(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    if request.method == 'POST':
+        tarefa.delete()
+        return redirect('lista_tarefas')
+    return render(request, 'todo_app/confirmar_exclusao.html', {'tarefa': tarefa})
