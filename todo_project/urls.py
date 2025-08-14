c1bd1a70fe0570a8dda3b04893f86b9fa5e87522
joseedusarmento <j.eduardo.sarmento10@gmail.com>
2025-08-14 16:00:36 -0300

    resolvendo pequeno erro de imports

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from tasks import views
 
 urlpatterns = [
     path('', views.lista_tarefa, name='lista_tarefas'),  # lista todas as tarefas
